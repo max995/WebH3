4a246d138d3c7445dc8f61bd8ae552121d4aa527
max995 <max995@126.com>
2019-04-29 23:25:44 +1000

    ZERO

---
+++ H3.py
@@ -0,0 +1,65 @@
+###hangman
+def hangman(secret_word, guesser, max_mistakes=8, verbose=True, **guesser_args):
+    """
+        This function plays the hangman game with the provided gusser and returns the number of incorrect guesses.
+
+        secret_word: a string of lower-case alphabetic characters, i.e., the answer to the game
+        guesser: a function which guesses the next character at each stage in the game
+            The function takes a:
+                mask: what is known of the word, as a string with _ denoting an unknown character
+                guessed: the set of characters which already been guessed in the game
+                guesser_args: additional (optional) keyword arguments, i.e., name=value
+        max_mistakes: limit on length of game, in terms of allowed mistakes
+        verbose: be chatty vs silent
+        guesser_args: keyword arguments to pass directly to the guesser function
+    """
+    secret_word = secret_word.lower()
+    mask = ['_'] * len(secret_word)
+    guessed = set()
+    if verbose:
+        print("Starting hangman game. Target is", ' '.join(mask), 'length', len(secret_word))
+
+    mistakes = 0
+    while mistakes < max_mistakes:
+        if verbose:
+            print("You have", (max_mistakes - mistakes), "attempts remaining.")
+        guess = guesser(mask, guessed, **guesser_args)
+
+        if verbose:
+            print('Guess is', guess)
+        if guess in guessed:
+            if verbose:
+                print('Already guessed this before.')
+            mistakes += 1
+        else:
+            guessed.add(guess)
+            if guess in secret_word:
+                for i, c in enumerate(secret_word):
+                    if c == guess:
+                        mask[i] = c
+                if verbose:
+                    print('Good guess:', ' '.join(mask))
+            else:
+                if verbose:
+                    print('Sorry, try again.')
+                mistakes += 1
+
+        if '_' not in mask:
+            if verbose:
+                print('Congratulations, you won.')
+            return mistakes
+
+    if verbose:
+        print('Out of guesses. The word was', secret_word)
+    return mistakes
+def human(mask, guessed, **kwargs):
+    """
+    This is a simple function for manual play.
+    """
+    print('Enter your guess:')
+    return input().lower().strip()
+interactive = True
+###If you want to play hangman interactively, please set `interactive` to True. When submitting your solution, set to False so we can automatically run the whole notebook using `Run All`.
+
+if interactive:
+    hangman('whatever', human, 8, True)
